a6b71a358994b2837b8774337ba274aa1562dbe3
Alicia <arrowsrp@yahoo.com>
2022-07-02 14:15:58 -0700

    commit tree 2847e3ca318757df28029f3b6b639e3525146d63

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,42 @@
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3, :sides
+
+  def initialize (side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+    validate([side1, side2, side3])
+  end
+
+  def kind
+    if self.side1 == self.side2 && self.side1 == self.side3
+      :equilateral
+    elsif self.side1 == self.side2 || self.side1 == self.side3 || self.side2 == self.side3
+      :isosceles
+    elsif self.side1 != self.side2 || self.side1 != self.side3 || self.side2 != self.side3
+      :scalene
+    elsif self.side1 == 0 && self.side2 == 0 && self.side3 == 0
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
+  
+
+  private
+
+  def validate array
+    if array.include?(0)
+      raise TriangleError
+    elsif array.all?(&:positive?) == false
+      raise TriangleError
+    elsif array[0] + array[1] <= array[2] || array[0] + array[2] <= array[1] || array[1] + array[2] <= array[0]
+      raise TriangleError
+    end
+  end
+
 end
+
+
